c8f043e384260e914d38e7f41b4a1785892be673
david omage <davidomage@yahoo.com>
2023-06-09 18:30:16 +0100

    modified email settings

--- event_reg/settings.py
+++ event_reg/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env = environ.Env()
+#reading .env file
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'davillamags@gmail.com'
-EMAIL_HOST_PASSWORD = 'htezdunrwstzihro'
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
 PASSWORD_RESET_TIMEOUT = 14400
